888d167a936603399572362124db1ec341cad696
shahdMohammad <shahdmohammadx@gmail.com>
2025-10-31 12:10:44 +0200

    feat: add Header Component

--- cibapp/src/components/layout/Header.jsx
+++ cibapp/src/components/layout/Header.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./Header.css"
 import heroimg from "../../assets/heroimage.svg"
+import Button from '../common/Button';
 const Header = () => {
     return (
         <div className='heroSection'>
@@ -8,6 +9,7 @@ const Header = () => {
                 <p className='heroTitle'>CIB</p>
                 <p className='heroSub'>Delivering value to our clients, our 
 community and our shareholders</p>
+            <Button buttonName="Discover More"/>
             </div>
             <img src={heroimg} alt="" className='imageh'/>
         </div>
